5d588e1dbc1e01c9190f830ff67263f0f063b43b
YouYiLucifer <523911584@qq.com>
2018-03-04 00:51:38 +0800

    组件迁移

--- src/components/Content/Content.vue
+++ src/components/Content/Content.vue
@@ -1,5 +1,16 @@
 <template>
-  
+  <div class="right-wrapper">
+    <input ref="input"
+           class="newTitle"
+           type="text"
+           placeholder="标题..."
+           :value="titleTarget"
+           style="outline: none">
+    <div class="editor"
+         placeholder="开始你的清单..."
+         contenteditable="true"
+         style="outline: none; cursor: text">{{ content }}</div>
+  </div>
 </template>
 <script>
 export default {
@@ -7,5 +18,29 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-
+.right-wrapper
+  flex 1
+  color #ccc
+  font-size 20px
+  display flex
+  flex-direction column
+  .newTitle
+    box-sizing border-box
+    width 100%
+    padding 36px 30px 20px
+    color rgb(130, 130, 130)
+    border-bottom 1px solid rgb(230, 230, 230)
+  .editor
+    flex 1
+    box-sizing border-box
+    padding 20px 30px
+    width 100%
+    height 100%
+    &:empty:before
+      content attr(placeholder)
+      font-size 16px
+      color rgb(200, 200, 200)
+    &:focus
+      content none
+      color rgb(130, 130, 130)
 </style>
--- src/components/Tab/Tab.vue
+++ src/components/Tab/Tab.vue
@@ -1,5 +1,40 @@
 <template>
-  
+  <div class="left-wrapper">
+    <div class="user">
+      <a href="#" class="avatar-wrapper">
+        <img src="./avatar.jpg" class="avatar">
+      </a>
+      <p class="name">Lucifer的APP</p>
+    </div>
+    <div class="menu">
+      <ul>
+        <li class="menu-item">
+          <router-link to="/all">
+            <span class="icon-files-empty"></span>
+            <span class="text">所有</span>
+            <span class="count">0</span>
+          </router-link>
+        </li>
+        <li class="menu-item">
+          <router-link to="/finished">
+            <span class="icon-checkmark2"></span>
+            <span class="text">已完成</span>
+            <span class="count">0</span>
+          </router-link>
+        </li>
+        <li class="menu-item">
+          <router-link to="/deleted">
+            <span class="icon-trash"></span>
+            <span class="text">垃圾桶</span>
+            <span class="count">0</span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="vip">
+      <a href="#" class="upgrade">升级到高级账户</a>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -7,5 +42,70 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-
+.left-wrapper
+  flex 0 0 260px
+  width 260px
+  background rgb(80, 130, 225)
+  display flex
+  flex-direction column
+  height 100vh
+  .user
+    box-sizing border-box
+    padding 16px 16px
+    font-size 0
+    color #fff
+    border-bottom 1px solid rgb(62, 116, 216)
+    .avatar-wrapper
+      display inline-block
+      width 40px
+      height 40px
+      margin-right 10px
+      .avatar
+        width 40px
+        height 40px
+        border-radius 50%
+    .name
+      display inline-block
+      vertical-align top
+      line-height 40px
+      font-size 18px
+      font-weight 500
+  .menu
+    flex 1 0 auto
+    border-bottom 1px solid rgb(62, 116, 216)
+    .menu-item
+      a
+        display block
+        padding 16px 20px
+        box-sizing border-box
+        position relative
+        text-decoration none
+        color #fff
+        &.active
+          background rgb(62, 116, 216)
+        &:hover
+          background rgb(62, 116, 216)
+        .icon-files-empty, .icon-checkmark2, .icon-trash
+          display inline-block
+          margin-right 6px
+          vertical-align top
+          font-size 16px
+        .text
+          display inline-block
+          vertical-align middle
+          font-size 14px
+        .count
+          display inline-block
+          position absolute
+          right 20px
+          vertical-align middle
+          font-size 14px
+  .vip
+    width 100%
+    height 60px
+    .upgrade
+      display block
+      line-height 60px
+      text-align center
+      color #fff
 </style>
